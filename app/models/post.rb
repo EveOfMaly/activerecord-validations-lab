d70640660a32b991c3c6a1f914bc30fd89b37911
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-02-27 14:45:01 -0500

    Completed most of lab except clickbait problem

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+        validates :title, presence: true
+        validates :content, length: { minimum: 250 }
+        validates :summary, length: { maximum: 250 }
+        validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+        validate :clickbait
+
+
+        def is_clickbait?
+            clickbate = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+
+            if clickbate.include?(:title)
+                errors.add(:title, "can't be a clickbate word")
+            end
+        end
+
 end
+
+
